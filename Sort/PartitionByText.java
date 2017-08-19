67349b384ce02af968bf265dcf8e14612e5d4b82
Nana0606 <paopaotanglee@126.com>
2017-08-19 22:23:28 +0800

    Create PartitionByText.java

---
+++ Sort/PartitionByText.java
@@ -0,0 +1,15 @@
+package com.itcast.hadoop.sort;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class PartitionByText extends Partitioner<IntPaire, IntWritable>{
+
+	@Override
+	public int getPartition(IntPaire key, IntWritable value, int numPartition) { //numPartition为reducer的个数
+		// TODO Auto-generated method stub
+		return (key.getFirstKey().hashCode() & Integer.MAX_VALUE) % numPartition;
+	}
+	
+
+}
