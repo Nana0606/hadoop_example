f33b26976fba9a0dc491d9325b5031f4f96ee130
Nana0606 <paopaotanglee@126.com>
2017-08-19 22:24:21 +0800

    Create TextComparator.java

---
+++ Sort/TextComparator.java
@@ -0,0 +1,21 @@
+package com.itcast.hadoop.sort;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class TextComparator extends WritableComparator{
+	
+	public TextComparator(){
+		super(IntPaire.class, true);
+	}
+
+	@Override
+	@SuppressWarnings("all")
+	public int compare(WritableComparable a, WritableComparable b) {
+		// TODO Auto-generated method stub
+		IntPaire o1 = (IntPaire) a;
+		IntPaire o2 = (IntPaire) b;
+		return o1.getFirstKey().compareTo(o2.getFirstKey());
+	}
+
+}
