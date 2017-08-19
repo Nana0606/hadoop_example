c3863c1a12729e4abfd9c24b4aaac56f05cf2fc9
Nana0606 <paopaotanglee@126.com>
2017-08-19 22:06:21 +0800

    Create CommonReduce.java

---
+++ Join/CommonReduce.java
@@ -0,0 +1,24 @@
+package com.itcast.hadoop.join;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class CommonReduce extends Reducer<TextPair, Text, Text, Text> {
+	
+	@Override
+	protected void reduce(TextPair key, Iterable<Text> values, Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("进入reduce");
+		String tradeId = values.iterator().next().toString(); //first value is tradeID
+		System.out.println(tradeId);
+		while(values.iterator().hasNext()){
+			//next is payID
+			String payID = values.iterator().next().toString();
+			context.write(new Text(tradeId), new Text(payID));
+		}
+	}
+
+}
