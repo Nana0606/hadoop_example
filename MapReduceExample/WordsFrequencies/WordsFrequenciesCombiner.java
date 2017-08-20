1e266b417a2f22a4b269bb4512b14c4d0179e8f4
Nana0606 <paopaotanglee@126.com>
2017-08-20 13:52:42 +0800

    Create WordsFrequenciesCombiner.java

---
+++ MapReduceExample/WordsFrequencies/WordsFrequenciesCombiner.java
@@ -0,0 +1,23 @@
+package com.oserp.wordsfrequencies;
+
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text; 
+
+public class WordsFrequenciesCombiner extends org.apache.hadoop.mapreduce.Reducer<Text, LongWritable,Text,LongWritable>{
+
+	@Override
+	protected void reduce(Text key, Iterable<LongWritable> values,
+			Context context)
+			throws IOException, InterruptedException {
+		
+		long counts = 0;
+
+		System.out.printf("\r[Combiner的输入键值] " + "Key:%s\t\t\tValue:",key);
+		for (LongWritable value : values) {
+			counts += value.get();	
+			System.out.printf("%s  ",value.get());
+		}
+		context.write(key, new LongWritable(counts));		
+	}
+}
