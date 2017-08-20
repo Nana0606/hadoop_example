713a76837e37823801b2d7706913aa3340dd6ec5
Nana0606 <paopaotanglee@126.com>
2017-08-20 13:53:05 +0800

    Create WordsFrequenciesMapper.java

---
+++ MapReduceExample/WordsFrequencies/WordsFrequenciesMapper.java
@@ -0,0 +1,27 @@
+package com.oserp.wordsfrequencies;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordsFrequenciesMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		String line = value.toString().trim();
+		
+		if (line.length() == 0)
+		{
+			return;
+		}
+		
+		String[] wordStrings = line.split("\\s+");
+		for (String word : wordStrings) {			
+			context.write(new Text(word.toLowerCase()), new LongWritable(1));			
+		}
+	} 
+
+}
