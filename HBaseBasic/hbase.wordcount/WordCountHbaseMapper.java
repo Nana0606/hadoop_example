c8a8158757a1c489bb900a408458f549ac541077
Nana0606 <paopaotanglee@126.com>
2017-08-21 21:38:29 +0800

    Create WordCountHbaseMapper.java

---
+++ HBaseBasic/hbase.wordcount/WordCountHbaseMapper.java
@@ -0,0 +1,25 @@
+package com.hbase.wordcount;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountHbaseMapper extends Mapper<Object, Text, Text, IntWritable> {
+	
+	private Text word = new Text();
+
+	@Override
+	protected void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		StringTokenizer iTokenizer = new StringTokenizer(value.toString());
+		while(iTokenizer.hasMoreTokens()){
+			word.set(iTokenizer.nextToken());
+			context.write(word, new IntWritable(1));
+		}
+	}	
+
+}
